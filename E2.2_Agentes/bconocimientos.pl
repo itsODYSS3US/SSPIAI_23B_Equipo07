532c256e2244e884921a6813a7753c372179ef74
itsODYSS3US <isaac.ascencio4173@alumnos.udg.mx>
2023-09-22 12:22:00 -0600

    E2.2

---
+++ E2.2_Agentes/bconocimientos.pl
@@ -0,0 +1,13 @@
+:- dynamic elemento/2.
+
+elemento(hidrogeno, h).
+elemento(helio, he).
+elemento(litio, li).
+elemento(berilio, be).
+elemento(boro, b).
+elemento(carbono, c).
+elemento(nitrogeno, n).
+elemento(oxigeno, o).
+elemento(cobalto, co).
+elemento(cobre, cu).
+
