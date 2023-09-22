22b4c4182869ee73fdcdfa9666320c0ccad3ec27
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-09-22 13:56:03 -0600

    [E2.2] Modifcación en las funciones y correción de warnings.
    Co-authored-by: itsODYSS3US <isaac.ascencio4173@alumnos.udg.mx>
    Co-authored-by: EDillon05 <cesar.dillon1275@alumnos.udg.mx>

--- E2.2_Agentes/bconocimientos.pl
+++ E2.2_Agentes/bconocimientos.pl
@@ -3,11 +3,8 @@
 elemento(hidrogeno, h).
 elemento(helio, he).
 elemento(litio, li).
-elemento(berilio, be).
-elemento(boro, b).
 elemento(carbono, c).
 elemento(nitrogeno, n).
 elemento(oxigeno, o).
-elemento(cobalto, co).
-elemento(cobre, cu).
+elemento(calsio, ca).
 
--- E2.2_Agentes/consultas.pl
+++ E2.2_Agentes/consultas.pl
@@ -3,6 +3,7 @@ bc :- consult('bconocimientos.pl').
 %C :- Símbolo del elemento
 
 inicio :-
+nl,
 write('---Menu---'), nl,
 write('1. Mostrar Base de conocimientpo'), nl,
 write('2. Agregar nuevo hecho'), nl,
@@ -10,7 +11,7 @@ write('3. Eliminar hecho'), nl,
 write('4. Buscar hecho'), nl,
 write('5. Modificar'), nl,
 write('6. Salir'), nl,
-write('Ingrese una opcion (1-6): '),
+write('Ingrese una opcion (1-6): '), nl,
 read(OP),
 opcion(OP).
 
@@ -20,26 +21,50 @@ opcion(2) :- agregar, inicio.
 opcion(3) :- eliminar, inicio.
 opcion(4) :- buscar, inicio.
 opcion(5) :- modificar, inicio.
-opcion(6) :- !.
-opcion(_):- write('Ingrese un dato valido'), nl, nl,inicio.
+opcion(6) :- write('Fin de programa...'), !.
+opcion(_):- write('Ingrese un dato valido'), nl, nl, inicio.
 
 %Mostrar
-mostrar :- listing(elemento/2).
+mostrar :- listing(elemento/2), nl.
 
 %Agregar
-guardar :- tell('bconocimientos.pl'), listing(elemento/2), told, write('Guardado...').
-agregar :- write("Escriba el elemento y su simbolo, al final poner un punto"), nl, read(N), read(C), assert(elemento(N,C)), guardar.
+    %Guardar los cambios en la base de conocimiento
+guardar :- tell('bconocimientos.pl'), listing(elemento/2), told.
+    %Agregar elemento insertado por el usuario
+
+agregar :- write("Escriba el elemento: "), nl, read(N), agregar(N).
+
+agregar(N) :- \+elemento(N,_) , write("Escriba el simbolo: "), nl, read(C), assert(elemento(N,C)), guardar , write('Gardado...'), nl.
+agregar(N) :- elemento(N,_) , write("El elemento que desea agregar ya existe"), nl.
+
 
 %Eliminar
-eliminar :- write("Escriba el elemento a borrar: "), nl, read(N), retract(elemento(N,C)), guardar.
-eliminar :- write("Escriba el elemento a borrar: "), nl , read(C), retract(elemento(N,C)), guardar.
+eliminar :- write('Escriba el elemento que desea borrar: '), nl, read(N), eliminar(N).
+
+eliminar(N) :- elemento(N,_), retract(elemento(N,_)), guardar, write('Eliminado...'), nl.
+eliminar(N) :- elemento(_,N), retract(elemento(_,N)), guardar, write('Eliminado...'), nl.
+
+eliminar(N) :- \+elemento(N,_), write('No se puede eliminar porque el elemento no se encuentra...'), nl.
+eliminar(N) :- \+elemento(_,N), write('No se puede eliminar porque el elemento no se encuentra...'), nl.
 
 %Buscar
-buscar :- write("Escriba el elemento o simbolo a buscar: "), read(N), buscar(N).
-buscar(N) :- elemento(N,C), write('Elemento encontrado'), nl, write(N), write(' su simbolo es: '), write(C) ; elemento(C,N), write('Valor encontrado'), nl, write(N), write(' su simbolo es: '), write(C).
-buscar(N) :- \+elemento(N,_), write('Elemento no encontrado') ; \+elemento(_,N), write('Simbolo no encontrado').
+buscar :- write("Escriba el elemento o simbolo a buscar: "), read(N), buscar(N,_).
+    %En caso de encontrar el elemento
+buscar(N,C) :- elemento(N,C), write('Elemento encontrado'), nl, write(N), write(' su simbolo es: '), write(C), nl; elemento(C,N), write('Valor encontrado'), nl, write(N), write(' su simbolo es: '), write(C), nl.
+    %En caso de no encontrar el elemento
+buscar(N,C) :- \+elemento(N,C), write('Elemento no encontrado'), nl, write("¿Deseas guardarlo? Si(s), No(n)"), nl, read(R), opMod(R) ; \+elemento(C,N), write('Simbolo no encontrado'), write("¿Deseas guardarlo? Si(s), No(n)"), nl, read(R), opMod(R).
+    %Switch para regresar al inicio o agregar elemento
+opMod(s):- agregar.
+opMod(n):- write("Regresando al inicio..."), nl, nl,inicio.
 
 %Modificar
-modificar :- write("Que elemento deseas borrar: ") , read(N), elemento(N,C), retract(elemento(N,C)), agregar.
-modificar :- write("Que elemento deseas borrar: ") , read(N), \+elemento(N,C), write("El elemento no se puede modificar porque no existe en la base de conocimiento"), nl, modificar.
+    %Elemento encontrado a modificar
+modificar :- write("Que elemento deseas modificar?: ") , nl, read(N), modificar(N).
+
+modificar(N) :-  elemento(N,_), retract(elemento(N,_)), agregar.
+modificar(N) :-  elemento(_,N), retract(elemento(_,N)), agregar.
+    %Elemento no se encuentra en la base de conocimiento
+modificar(N) :- \+elemento(N,_), write("El elemento no se puede modificar porque no existe en la base de conocimiento"), nl.
+modificar(N) :- \+elemento(_,N), write("El elemento no se puede modificar porque no existe en la base de conocimiento"), nl.
+
 
