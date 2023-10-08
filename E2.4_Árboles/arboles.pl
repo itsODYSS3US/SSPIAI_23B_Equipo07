845ad59fff77fb9e4fbb49b88f8007752ecd3b36
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2023-10-07 18:20:03 -0600

    Método contar hojas terminado.
    Co-Authored-By: EDillon05 <cesar.dillon1275@alumnos.udg.mx>
    Co-Authored-By: itsODYSS3US <isaac.ascencio1275@alumnos.udg.mx>

--- E2.4_Árboles/arboles.pl
+++ E2.4_Árboles/arboles.pl
@@ -1,9 +1,4 @@
-% Buscar
-% busqueda :- pregunta, read(VB), busPre(arbol,VB),
-% busPre(arbol(R,I,D),B) :- R == B, !,
-%                           buscarPre(I),
-%                           buscarPre(D).
-
+%%Busqueda en un árbol%%
 arbol(+,arbol(x,3,5),arbol(-,2,5)).
 
 buscar(N,arbol(N,_,_)) :- !.
@@ -13,10 +8,7 @@ buscar(E,arbol(N,_,_)) :- E \= N,
     
 %buscar(3,arbol(+,arbol(x,3,5),arbol(-,2,5))).
 
-% preorden(arbol(Raiz,Hizq,Hder)):- write(Raiz), preorden(Hizq), preorden(Hder).
-% preorden(H):- write(H).
-
-%Contar
+%Contar Nodos
 arbolB(+,arbolB(x,arbolB(3,vacio,vacio),arbolB(5,vacio,vacio)),arbolB(-,arbolB(2,vacio,vacio),arbolB(5,vacio,vacio))).
 
 contar(vacio, 0).
@@ -27,20 +19,17 @@ contar(arbolB(_, I, D), L) :-
 
 %contar(arbolB(+,arbolB(x,arbolB(3,vacio,vacio),arbolB(5,vacio,vacio)),arbolB(-,arbolB(2,vacio,vacio),arbolB(5,vacio,vacio))) ,L).
 
-%deArbolALista(arbol(R,I,D),listaRes).
 %Calcular/conocer la profundidad del árbol
-
 arbolC(+,arbolC(x,arbolC(3,arbolC(4,arbolC(6,vacio,vacio),vacio),vacio),arbolC(5,vacio,vacio)),arbolC(-,arbolC(2,vacio,vacio),arbolC(5,vacio,vacio))).
 profundidad(vacio,0).
 profundidad(arbolC(_,I,D),P) :-
 	profundidad(I,PI),
 	profundidad(D,PD),
-	P is 1+max(PI,PD).
+	P is 1 + max(PI,PD).
 
-%
+%profunidad(arbolC(+,arbolC(x,arbolC(3,arbolC(4,arbolC(6,vacio,vacio),vacio),vacio),arbolC(5,vacio,vacio)),arbolC(-,arbolC(2,vacio,vacio),arbolC(5,vacio,vacio))),P).
 
-%Lista
-
+%%De Árbola a Lista
 arbolD(+,arbolD(x,arbolD(3,arbolD(4,arbolD(6,vacio,vacio),vacio),vacio),arbolD(5,vacio,vacio)),arbolD(-,arbolD(2,vacio,vacio),arbolD(5,vacio,vacio))).
 
 lista(vacio, []).
@@ -48,10 +37,18 @@ lista(arbolD(Raiz, Izquierda, Derecha), Lista) :-
     lista(Izquierda, ListaIzquierda),
     lista(Derecha, ListaDerecha),
     append(ListaIzquierda,[Raiz|ListaDerecha], Lista).
+    
 %lista(arbolD(+,arbolD(x,arbolD(3,arbolD(4,arbolD(6,vacio,vacio),vacio),vacio),arbolD(5,vacio,vacio)),arbolD(-,arbolD(2,vacio,vacio),arbolD(5,vacio,vacio))) ,L).
 
 
-%Contar la cantidad de hojas
+%%Contar la cantidad de hojas%%
+arbolF(+,arbolF(x,arbolF(3,arbolF(4,arbolF(6,arbolF(4,hoja,hoja),hoja),hoja),hoja),arbolF(5,arbolF(4,hoja,hoja),hoja)),arbolF(-,arbolF(2,hoja,hoja),arbolF(5,hoja,hoja))).
 
+contar_hojas(hoja, 1).
+contar_hojas(arbolF(_, Izquierda, Derecha), Conteo) :-
+    contar_hojas(Izquierda, ConteoIzquierda),
+    contar_hojas(Derecha, ConteoDerecha),
+    Conteo is ConteoIzquierda + ConteoDerecha.
 
-%lista([raiz,[Hizq],[Hder]])izq  
+%contar_hojas(arbolF(+,arbolF(x,arbolF(3,arbolF(4,arbolF(6,arbolF(4,hoja,hoja),hoja),hoja),hoja),arbolF(5,arbolF(4,hoja,hoja),hoja)),arbolF(-,arbolF(2,hoja,hoja),arbolF(5,hoja,hoja))), C).
+
