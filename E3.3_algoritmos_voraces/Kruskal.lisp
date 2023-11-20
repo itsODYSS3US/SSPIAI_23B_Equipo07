7ab82e071203142194f3c486047e7b2ab539dbde
itsODYSS3US <isaac.ascencio4173@alumnos.udg.mx>
2023-11-20 15:20:14 -0600

    E3.3 -  kruskal no terminado
    Co-authored-by: Saul <SaulMerc@users.noreply.github.com>

--- E3.3_algoritmos_voraces/Kruskal.lisp
+++ E3.3_algoritmos_voraces/Kruskal.lisp
@@ -10,6 +10,25 @@
     (m ((f 3)))
 ))
 
+(defun crear-grafo ()
+  (let ((grafo '()))
+    (format t "Creando un nuevo grafo.~% Ingrese los vértices y sus conexiones con pesos.~% Ingrese 'fin' para terminar.~%")
+    (loop
+      (format t "Ingrese el nombre del vértice (o 'fin' para terminar): ")
+      (let ((vertice (read-line)))
+        (if (equal vertice "fin")
+            (return (reverse grafo))
+            (progn
+              (format t "Ingrese las conexiones y pesos en el formato '(vértice peso)' separados por espacios (o 'fin' para terminar): ")
+              (let ((conexiones (parsear-conexiones (read-line))))
+                (push (list vertice conexiones) grafo))))))))
+
+(defun parsear-conexiones (conexiones-str)
+  (let ((parsed-connections '()))
+    (loop for pair in (read-from-string (concatenate 'string "(" conexiones-str ")"))
+          do (push (list (first pair) (second pair)) parsed-connections))
+    parsed-connections))
+
 ;Obtener los nodos adyacentes del grafo
 (defun adyacentes (nodo graph)
     (cadr (assoc nodo graph))
@@ -40,7 +59,6 @@
 (defun quitarnil (lista)
 (mapcar #'(lambda (x) (remove-if #'null x)) lista))
 
-; (setf quit (quitarnil lista))
 
 ; ((M (F 3)) (I (D 4)) (I (A 3)) (F (M 3)) (F (E 4)) (E (B 5)) (E (F 4)) (E (D 2)) (D (E 2)) (D (A 5)) (D (I 4)) (C (B 4)) (B (E 5)) (B (C 4)) (B (A 4)) (A (D 5)) (A (B 4)) (A (I 3)))
 
@@ -60,71 +78,66 @@
 
 ;((E (D 2) NIL) (D (E 2) NIL) (M (F 3) NIL) (I (A 3) NIL) (F (M 3) NIL) (A (I 3) NIL) (I (D 4) NIL) (F (E 4) NIL) (E (F 4) NIL) (D (I 4) NIL) (C (B 4) NIL) (B (C 4) NIL) (B (A 4) NIL) (A (B 4) NIL) (E (B 5) NIL) (D (A 5) NIL) (B (E 5) NIL) (A (D 5) NIL))
 
-; (setf quit (quitarnil lista))
+(defun krusk ()
+    (setq grafo (crear-grafo))
+    ;Genera las aristas y elimina el valor nil
+    (setq aristas (listaRes grafo))
+    (setq aristasn (quitarnil aristas))
+    ;Ordenar, dar formato y quitar valor nil
+    (setq aris-sort (lista-ordenada aristasn))
+    (setq aris-sortn (quitarnil aris-sort))
+  )
 
-; (defun kruskal (grafo)
-;     ;Genera las aristas y elimina el valor nil
-;     (setq aristas (listaRes grafo))
-;     (setq aristasn (quitarnil aristas))
-;     ;Ordenar, dar formato y quitar valor nil
-;     (setq aris-sort (lista-ordenada aristasn))
-;     (setq aris-sortn (quitarnil aris-sort))
-;     aris-sortn
-; )
+;(M (F 3))
+;jala a medias xd
+(defun encontrar (x padre)
+  "Encuentra el conjunto al que pertenece el nodo X."
+  (if (eq x (car padre))
+      x
+      (encontrar (car padre) padre)))
 
+(defun uni (x y padre)
+  "Une dos conjuntos x e y."
+  (let ((raizX (encontrar x padre))
+        (raizY (encontrar y padre)))
+    (setf (car (member raizX padre)) raizY)))
 
-; (setq ordenado '((E (D 2)) (D (E 2)) (M (F 3)) (I (A 3)) (F (M 3)) (A (I 3)) (I (D 4)) (F (E 4))
-;                  (E (F 4)) (D (I 4)) (C (B 4)) (B (C 4)) (B (A 4)) (A (B 4)) (E (B 5)) (D (A 5))
-;                  (B (E 5)) (A (D 5))))
+(defun kruskal (edges)
+  "Algoritmo de Kruskal para encontrar un árbol de expansión mínima."
+  (let* ((sorted-edges (sort edges #'< :key #'caddr))
+        (resultado '())
+        (padre (make-hash-table :test #'equal)))
+    (dolist (edge sorted-edges)
+      (let ((u (first edge))
+            (v (second edge))
+            (peso (third edge)))
+        (unless (and (gethash u padre)
+                    (gethash v padre)
+                    (equal (gethash u padre) (gethash v padre)))
+          (push edge resultado)
+          (setf (gethash u padre) u)
+          (setf (gethash v padre) u))))
+    resultado))
 
+(setq ordenado '((E D 2) (D E 2) (M F 3) (I A 3) (F M 3) (A I 3) (I D 4) (F E 4)
+                (E F 4) (D I 4) (C B 4) (B C 4) (B A 4) (A B 4) (E B 5) (D A 5)
+                (B E 5) (A D 5)))
 
-; ((D E 2) (I A 3) (C B 4) (E F 4) (F M 3) (A B 4))
+(setq resultado (kruskal ordenado))
 
-; (defun encontrar (x padre)
-;   "Encuentra el conjunto al que pertenece el nodo X."
-;   (if (eq x (car padre))
-;       x
-;       (encontrar (car padre) padre)))
-
-; (defun uni (x y padre)
-;   "Une dos conjuntos x e y."
-;   (let ((raizX (encontrar x padre))
-;         (raizY (encontrar y padre)))
-;     (setf (car (member raizX padre)) raizY)))
-
-; (defun kruskal (edges)
-;   "Algoritmo de Kruskal para encontrar un árbol de expansión mínima."
-;   (let* ((sorted-edges (sort edges #'< :key #'caddr))
-;          (resultado '())
-;          (padre (make-hash-table :test #'equal)))
-;     (dolist (edge sorted-edges)
-;       (let ((u (first edge))
-;             (v (second edge))
-;             (peso (third edge)))
-;         (unless (and (gethash u padre)
-;                      (gethash v padre)
-;                      (equal (gethash u padre) (gethash v padre)))
-;           (push edge resultado)
-;           (setf (gethash u padre) u)
-;           (setf (gethash v padre) u))))
-;     resultado))
-
-; (setq ordenado '((E D 2) (D E 2) (M F 3) (I A 3) (F M 3) (A I 3) (I D 4) (F E 4)
-;                  (E F 4) (D I 4) (C B 4) (B C 4) (B A 4) (A B 4) (E B 5) (D A 5)
-;                  (B E 5) (A D 5)))
-
-; (setq resultado (kruskal ordenado))
-
-; (format t "Árbol de expansión mínima (Kruskal):~%")
-; (dolist (edge resultado)
-;   (format t "~a -> ~a (~a)~%" (first edge) (second edge) (third edge)))
-
-
+(format t "Árbol de expansión mínima (Kruskal):~%")
+(dolist (edge resultado)
+  (format t "~a -> ~a (~a)~%" (first edge) (second edge) (third edge)))
 
 
 ; kruskal ordenado
 ; ((D A 5) (E B 5) (B A 4) (C B 4) (F E 4) (I D 4) (I A 3) (M F 3) (E D 2))
 ; ((B A 4) (C B 4) (F E 4) (I D 4) (I A 3) (M F 3) (E D 2))
+
+
+
+
+
 
 
 
